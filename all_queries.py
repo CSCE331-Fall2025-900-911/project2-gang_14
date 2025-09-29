881cd0bd737cfc81041b2cf9baaa2e9e28b03a8c
Siya Bhatkal <siyabhatkal2010@tamu.edu>
2025-09-29 14:45:33 -0500

    added my query to all_queries

--- all_queries.py
+++ all_queries.py
@@ -1,1 +1,23 @@
 #write queries here
+
+import psycopg2
+import random
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    host="csce-315-db.engr.tamu.edu",
+    database="gang_14_db",
+    user="gang_14",
+    password="gang_14",
+)
+cursor = conn.cursor()
+
+cursor.execute("""
+    SELECT 
+        EXTRACT(WEEK FROM order_date) AS week,
+        COUNT(order_id) AS orders
+    FROM orders
+    GROUP BY week
+    ORDER BY week;
+""") #extract week in postgreSQL and count the total orders
+
