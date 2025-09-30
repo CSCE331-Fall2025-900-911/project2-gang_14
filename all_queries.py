8cc08ba596dcd7459df406af8f7e1bce63c8c55b
Siya Bhatkal <siyabhatkal2010@tamu.edu>
2025-09-30 16:23:12 -0500

    Update all_queries.py

--- all_queries.py
+++ all_queries.py
@@ -11,7 +11,7 @@ conn = psycopg2.connect(
     password="gang_14",
 )
 cursor = conn.cursor()
-
+#extract week in postgreSQL and count the total orders
 cursor.execute("""
     SELECT 
         EXTRACT(WEEK FROM order_date) AS week,
@@ -19,5 +19,28 @@ cursor.execute("""
     FROM orders
     GROUP BY week
     ORDER BY week;
-""") #extract week in postgreSQL and count the total orders
+""") 
 
+#Display total sales grouped by each employee 
+cursor.execute("""
+    SELECT 
+        employee_name as employee_name ,
+        COUNT(order_id) AS orders
+    FROM orders o
+    join employee e on e.employee_id = o.employee_id
+    GROUP BY e.employee_name
+    ORDER BY orders DESC;
+    ;
+""") 
+
+# count the number of ingredients in a drink
+cursor.execute("""
+    SELECT 
+        p.product_name,
+        COUNT(di.ingredient_id) AS ingredients
+    FROM drink_ingredient di
+    join product p on o.product_id=di.product_id
+    group by p.product_name
+    order by p.product_name  
+    ;
+""") 
